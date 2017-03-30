324ae29b4f13f24541944d20de00a2e61bc9947d
J. Daniel Smith <public@jdanielsmith.name>
2017-03-29 22:06:02 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/JDanielSmith/CaseInsenstiveString.git""
    This reverts commit 111471936d78e6b5e927eaf437c2fa66cc57137b.

--- CaseInsensitiveString/String.cs
+++ CaseInsensitiveString/String.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using SystemStringComparer = System.StringComparer;
-using SystemStringComparison = System.StringComparison;
 
 using CodeAnalysis = System.Diagnostics.CodeAnalysis;
 
@@ -16,33 +14,26 @@ namespace JDanielSmith.System
 	/// </summary>
 	[CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "String")]
 	[CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1036:OverrideMethodsOnComparableTypes")]
-	public sealed class String<TStringComparison> : IComparable, ICloneable,
-		IComparable<String<TStringComparison>>, IEquatable<String<TStringComparison>>,
-		IComparable<string>, IEquatable<string>
-		where TStringComparison : StringComparison, new()
+	public sealed class String<TComparerAndComparison> : IComparable, ICloneable,
+		IComparable<String<TComparerAndComparison>>, IEquatable<String<TComparerAndComparison>>,
+		IComparable<String>, IEquatable<String>
+		where TComparerAndComparison : StringComparerAndComparison, new()
 	{
-		// Can't do TStringComparison.Comparer as in C++.  But since the new() constraint was provided
-		// create a new instance and extract the information.  This only needs to be done once per type.
-		static readonly StringComparison _instance = new TStringComparison();
-		static readonly SystemStringComparer _comparer = _instance.Comparer;
+		static readonly StringComparerAndComparison _comparerAndComparison = new TComparerAndComparison();
+		static readonly StringComparer _comparer = _comparerAndComparison.Comparer;
+		static readonly StringComparison _comparisonType = _comparerAndComparison.Comparison;
 
 		public string Value { get; }
 
 		public String(string value)
 		{
 			// matching the behavior of System.String is more straight-forward if "Value" is never null
-			Value = value ?? string.Empty;
+			Value = value ?? String.Empty;
 		}
 
 		// easily convert to/from System.String
-		public static implicit operator String<TStringComparison>(string value)
-		{
-			return new String<TStringComparison>(value);
-		}
-		public static implicit operator string(String<TStringComparison> value)
-		{
-			return value?.Value;
-		}
+		public static implicit operator String<TComparerAndComparison>(string source) => new String<TComparerAndComparison>(source);
+		public static implicit operator string(String<TComparerAndComparison> source) => source?.Value;
 
 		#region Equals, IEquatable
 		public override bool Equals(object obj)
@@ -50,7 +41,7 @@ namespace JDanielSmith.System
 			if (Object.ReferenceEquals(obj, null))
 				return false; // this != null
 
-			var other = obj as String<TStringComparison>;
+			var other = obj as String<TComparerAndComparison>;
 			if (!Object.ReferenceEquals(other, null))
 				return Equals(other); // call Equals(String<TStringComparerAndComparison>)
 
@@ -60,44 +51,19 @@ namespace JDanielSmith.System
 
 			return _comparer.Equals(obj);
 		}
-		public bool Equals(String<TStringComparison> other)
+		public bool Equals(String<TComparerAndComparison> other)
 		{
 			if (Object.ReferenceEquals(other, null))
 				return false; // this != null
 			return Equals(other.Value); // call Equals(string)
 		}
-		public bool Equals(String<TStringComparison> other, SystemStringComparison comparisonType) // for easier use with existing code
-		{
-			if (comparisonType != _comparisonType)
-				throw new ArgumentOutOfRangeException(nameof(comparisonType));
-			return Equals(other);
-		}
-		public bool Equals(string other)
-		{
-			return _comparer.Equals(Value, other);
-		}
-		public bool Equals(string other, SystemStringComparison comparisonType) // for easier use with existing code
-		{
-			if (comparisonType != _comparisonType)
-				throw new ArgumentOutOfRangeException(nameof(comparisonType));
-			return _comparer.Equals(Value, other);
-		}
-
-		public override int GetHashCode()
-		{
-			return _comparer.GetHashCode(Value);
-		}
+		public bool Equals(string other) => _comparer.Equals(Value, other);
+		public override int GetHashCode() => _comparer.GetHashCode(Value); 
 		#endregion
 
-		public override string ToString()
-		{
-			return Value;
-		}
+		public override string ToString() => Value;
 
-		public object Clone()
-		{
-			return new String<TStringComparison>(Value);
-		}
+		public object Clone() => new String<TComparerAndComparison>(Value);
 
 		#region IComparable
 		public int CompareTo(object obj)
@@ -107,19 +73,19 @@ namespace JDanielSmith.System
 				return 1; // If other is not a valid object reference, this instance is greater.
 
 			// obj must be either StringOrdinalIgnoreCase or String
-			var other = obj as String<TStringComparison>;
+			var other = obj as String<TComparerAndComparison>;
 			if (Object.ReferenceEquals(other, null))
 			{
 				var s_other = obj as string;
 				if (Object.ReferenceEquals(s_other, null))
-					throw new ArgumentException("Object must be of type " + nameof(String<TStringComparison>) + " or String.");
+					throw new ArgumentException("Object must be of type " + nameof(String<TComparerAndComparison>) + " or String.");
 
 				return CompareTo(s_other); // call CompareTo(string)
 			}
 
 			return CompareTo(other); // call CompareTo(StringOrdinalIgnoreCase)
 		}
-		public int CompareTo(String<TStringComparison> other)
+		public int CompareTo(String<TComparerAndComparison> other)
 		{
 			// https://msdn.microsoft.com/en-us/library/4d7sx9hd(v=vs.110).aspx
 			if (Object.ReferenceEquals(other, null))
@@ -139,128 +105,33 @@ namespace JDanielSmith.System
 			return _comparer.Compare(Value, other);
 		}
 
-		public static bool operator ==(String<TStringComparison> left, String<TStringComparison> right)
+		public static bool operator ==(String<TComparerAndComparison> x, String<TComparerAndComparison> y)
 		{
-			if (Object.ReferenceEquals(left, null))
-				return Object.ReferenceEquals(right, null); // null == null, null != something
-			return left.Equals(right); // know left != null
+			if (Object.ReferenceEquals(x, null))
+				return Object.ReferenceEquals(y, null); // null == null, null != something
+			return x.Equals(y); // know x != null
 		}
-		public static bool operator ==(String<TStringComparison> left, string right)
+		public static bool operator ==(String<TComparerAndComparison> x, string y)
 		{
-			if (Object.ReferenceEquals(left, null))
-				return Object.ReferenceEquals(right, null); // null == null, null != something
-			return left.Equals(right); // know x != null
+			if (Object.ReferenceEquals(x, null))
+				return Object.ReferenceEquals(y, null); // null == null, null != something
+			return x.Equals(y); // know x != null
 		}
-		public static bool operator ==(string left, String<TStringComparison> right)
-		{
-			return right == left; // == is commutative, left == right
-		}
-
-		public static bool operator !=(String<TStringComparison> left, String<TStringComparison> right)
-		{
-			return !(left == right);
-		}
-		public static bool operator !=(string left, String<TStringComparison> right)
-		{
-			return !(left == right);
-		}
-		public static bool operator !=(String<TStringComparison> left, string right)
-		{
-			return !(left == right);
-		}
+		public static bool operator ==(string x, String<TComparerAndComparison> y) => y == x; // == is commutative, x == y
+		public static bool operator !=(String<TComparerAndComparison> x, String<TComparerAndComparison> y) => !(x == y);
+		public static bool operator !=(string x, String<TComparerAndComparison> y) => !(x == y);
+		public static bool operator !=(String<TComparerAndComparison> x, string y) => !(x == y);
 		#endregion
 
 		#region IndexOf, LastIndexOf, StartsWith, EndsWith
-		static readonly SystemStringComparison _comparisonType = _instance.Comparison;
-
-		public bool EndsWith(string value)
-		{
-			return Value.EndsWith(value, _comparisonType);
-		}
-		public bool EndsWith(string value, SystemStringComparison comparisonType) // for easier use with existing code
-		{
-			if (comparisonType != _comparisonType)
-				throw new ArgumentOutOfRangeException(nameof(comparisonType));
-			return EndsWith(value);
-		}
-
-		public int IndexOf(string value)
-		{
-			return Value.IndexOf(value, _comparisonType);
-		}
-		public int IndexOf(string value, SystemStringComparison comparisonType) // for easier use with existing code
-		{
-			if (comparisonType != _comparisonType)
-				throw new ArgumentOutOfRangeException(nameof(comparisonType));
-			return IndexOf(value);
-		}
-
-		public int IndexOf(string value, int startIndex)
-		{
-			return Value.IndexOf(value, startIndex, _comparisonType);
-		}
-		public int IndexOf(string value, int startIndex, SystemStringComparison comparisonType) // for easier use with existing code
-		{
-			if (comparisonType != _comparisonType)
-				throw new ArgumentOutOfRangeException(nameof(comparisonType));
-			return IndexOf(value, startIndex);
-		}
-
-		public int IndexOf(string value, int startIndex, int count)
-		{
-			return Value.IndexOf(value, startIndex, count, _comparisonType);
-		}
-		public int IndexOf(string value, int startIndex, int count, SystemStringComparison comparisonType) // for easier use with existing code
-		{
-			if (comparisonType != _comparisonType)
-				throw new ArgumentOutOfRangeException(nameof(comparisonType));
-			return IndexOf(value, startIndex, count);
-		}
-
-		public int LastIndexOf(string value)
-		{
-			return Value.LastIndexOf(value, _comparisonType);
-		}
-		public int LastIndexOf(string value, SystemStringComparison comparisonType) // for easier use with existing code
-		{
-			if (comparisonType != _comparisonType)
-				throw new ArgumentOutOfRangeException(nameof(comparisonType));
-			return LastIndexOf(value);
-		}
-
-		public int LastIndexOf(string value, int startIndex)
-		{
-			return Value.LastIndexOf(value, startIndex, _comparisonType);
-		}
-		public int LastIndexOf(string value, int startIndex, SystemStringComparison comparisonType) // for easier use with existing code
-		{
-			if (comparisonType != _comparisonType)
-				throw new ArgumentOutOfRangeException(nameof(comparisonType));
-			return LastIndexOf(value, startIndex);
-		}
-
-		public int LastIndexOf(string value, int startIndex, int count)
-		{
-			return Value.LastIndexOf(value, startIndex, count, _comparisonType);
-		}
-		public int LastIndexOf(string value, int startIndex, int count, SystemStringComparison comparisonType) // for easier use with existing code
-		{
-			if (comparisonType != _comparisonType)
-				throw new ArgumentOutOfRangeException(nameof(comparisonType));
-			return LastIndexOf(value, startIndex, count);
-		}
-		
-		public bool StartsWith(string value)
-		{
-			return Value.StartsWith(value, _comparisonType);
-		}
-		public bool StartsWith(string value, SystemStringComparison comparisonType) // for easier use with existing code
-		{
-			if (comparisonType != _comparisonType)
-				throw new ArgumentOutOfRangeException(nameof(comparisonType));
-
-			return StartsWith(value);
-		}
+		public bool EndsWith(string value) => Value.EndsWith(value, _comparisonType);
+		public int IndexOf(string value) => Value.IndexOf(value, _comparisonType);
+		public int IndexOf(string value, int startIndex) => Value.IndexOf(value, startIndex, _comparisonType);
+		public int IndexOf(string value, int startIndex, int count) => Value.IndexOf(value, startIndex, count, _comparisonType);
+		public int LastIndexOf(string value) => Value.LastIndexOf(value, _comparisonType);
+		public int LastIndexOf(string value, int startIndex) => Value.LastIndexOf(value, startIndex, _comparisonType);
+		public int LastIndexOf(string value, int startIndex, int count) => Value.LastIndexOf(value, startIndex, count, _comparisonType);
+		public bool StartsWith(string value) => Value.StartsWith(value, _comparisonType);
 		#endregion
 
 	}
